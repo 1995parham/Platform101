cb71e857a8fb4866da71177b94b4edb02c71b027
Parham Alvani <parham.alvani@gmail.com>
2018-08-14 00:56:35 +0430

    Corrects index file.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-app.use(express.static('.'));
+app.use(express.static("."));
 
 app.listen(3000, () => {
-      console.log('App started on port 3000');
+      Console.log("App started on port 3000");
 });
